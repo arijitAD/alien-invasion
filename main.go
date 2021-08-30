2ab09884e4b24f067e81238525c2cfa32ed493c3
Arijit Das <arijitad.in@gmail.com>
2021-08-30 16:47:14 +0530

    Print in Golang

---
+++ main.go
@@ -0,0 +1,16 @@
+package alien_invasion
+
+import (
+	"fmt"
+	"os"
+)
+
+const (
+	title = "$RAW_TITLE"
+	body = "$RAW_BODY"
+)
+
+func main() {
+	fmt.Println("Title: ", os.Getenv(title))
+	fmt.Println("Body: ", os.Getenv(body))
+}
